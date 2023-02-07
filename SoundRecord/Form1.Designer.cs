83fcdd6d56c87d0baf70ba1721a191f2c155c7ff
ildaram <ildarminnull@gmail.com>
2023-02-07 08:59:40 +0300

    Добавьте файлы проекта.

---
+++ SoundRecord/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace SoundRecord
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.progressBar1 = new System.Windows.Forms.ProgressBar();
+			this.timer = new System.Windows.Forms.Timer(this.components);
+			this.SuspendLayout();
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(25, 13);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(294, 21);
+			this.comboBox1.TabIndex = 0;
+			// 
+			// progressBar1
+			// 
+			this.progressBar1.Location = new System.Drawing.Point(25, 60);
+			this.progressBar1.Name = "progressBar1";
+			this.progressBar1.Size = new System.Drawing.Size(294, 25);
+			this.progressBar1.TabIndex = 1;
+			// 
+			// timer
+			// 
+			this.timer.Enabled = true;
+			this.timer.Interval = 10;
+			this.timer.Tick += new System.EventHandler(this.timer_Tick);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(421, 450);
+			this.Controls.Add(this.progressBar1);
+			this.Controls.Add(this.comboBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.ProgressBar progressBar1;
+		private System.Windows.Forms.Timer timer;
+	}
+}
+
